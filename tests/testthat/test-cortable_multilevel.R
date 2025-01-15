5c0d8f5c67ea71a04b37b44ea1ac4c2473729f30
Francisco Wilhelm <38857150+franciscowilhelm@users.noreply.github.com>
2025-01-15 16:49:15 +0100

    updates to tests and examples

--- tests/testthat/test-cortable_multilevel.R
+++ tests/testthat/test-cortable_multilevel.R
@@ -1,3 +1,12 @@
 cortable_multilevel(mc_twolevel, c("Y", "M", "X"), "CLUSTER")
 cortable_multilevel(mc_twolevel, c("Y", "M", "X"), "CLUSTER", wpv = TRUE)
 cortable_multilevel(mc_twolevel, c("Y", "M", "X"), "CLUSTER", use.001 = FALSE)
+
+load("exampleData/df_example_cli.RData")
+library(dplyr)
+df_example_cli <- df_example_cli |> ungroup()
+
+cortable_integriert2 <- cortable_multilevel(df_example_cli,
+                                            varnames = c("w", "y", "x"),
+                                            grp = "id")
+cortable_integriert2
